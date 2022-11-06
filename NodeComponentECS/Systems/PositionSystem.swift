3e3b51c71d64a4fe64c3a0858091d223fc4a74bd
johnrnyquist <john@nyquist.net>
2022-11-06 08:35:09 -0700

    Refactor

--- NodeComponentECS/Systems/PositionSystem.swift
+++ NodeComponentECS/Systems/PositionSystem.swift
@@ -4,10 +4,10 @@ import GameplayKit
 class PositionSystem: GKComponentSystem<GKComponent> {
     override func addComponent(_ component: GKComponent) {
         super.addComponent(component)
-        guard let skNode = component.entity?.component(ofType: NodeComponent.self)?.node,
+        guard let node = component.entity?.component(ofType: NodeComponent.self)?.node,
               let component = component as? PositionComponent
         else { return }
-        skNode.position = component.currentPosition
+        node.position = component.currentPosition
     }
 
     override func addComponent(foundIn entity: GKEntity) {
